622babf75bc307c21e83b1df843fa0ca51315b71
ronanzindev <morellianogm@gmail.com>
2023-05-17 19:45:38 -0300

    more than 1 parameter

--- command-line-parsing/cli.ml
+++ command-line-parsing/cli.ml
@@ -1,12 +1,18 @@
 open Core
-let do_hash filename = 
-    Md5.digest_file_blocking filename |> Md5.to_hex |> print_endline
-let filename_param =
-    let open Command.Param in
-    anon ("filename" %: string)
 
-let command = 
-    Command.basic 
-        ~summary: "Generade an MD5 hash of the input data"
-        ~readme: (fun () -> "More detailed information")
-        (Command.Param.map filename_param ~f: (fun filename () -> do_hash filename))
+let do_hash hash_length filename =
+  Md5.digest_file_blocking filename |> Md5.to_hex |> (fun s -> String.prefix s hash_length) |> print_endline 
+
+let command =
+    Command.basic
+    ~summary:"Generate an MD5 hash of the input data"
+    ~readme:(fun () -> "More detailed information")
+    Command.Param.(
+      map
+        (both
+           (anon ("hash_length" %: int))
+           (anon ("filename" %: string)))
+        ~f:(fun (hash_length, filename) () ->
+          do_hash hash_length filename))
+
+let () = Command_unix.run ~version:"1.0" ~build_info:"RWO" command
